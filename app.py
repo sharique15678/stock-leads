3d5016892bb60adac6dd8fa13ec6dca87af3c86f
Your Name <you@example.com>
2021-02-20 22:21:41 +0530

    making ui files

--- app.py
+++ app.py
@@ -10,14 +10,61 @@ from datetime import date, timedelta
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "ff00ff"
 app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=30)
-
 db = SQLAlchemy(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
+
+class Leads(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    logo = db.Column(db.Text, nullable=True)
+    type = db.Column(db.String(10), nullable=False)
+    buy_price = db.Column(db.String(30), nullable=False)
+    sell_price = db.Column(db.String(30), nullable=False)
+    profit = db.Column(db.String(10), nullable=False)
+    good_for_trade = db.Column(db.Boolean, nullable=False, default=True)
+    date = db.Column(db.String(100), nullable=False, default=date.today().strftime("%d/%m/%Y"))
+
+class Users(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	fname = db.Column(db.String(50), nullable=False)
+	mname = db.Column(db.String(50), nullable=True , default="No_name_Provided")
+	lname = db.Column(db.String(50), nullable=False)
+	email = db.Column(db.String(100), nullable=False,unique=False)
+	add1 = db.Column(db.Text, nullable=False)
+	add2 = db.Column(db.Text, nullable=True,default="No Second Address")
+	city = db.Column(db.String(50), nullable=False)
+	state = db.Column(db.String(50), nullable=False)
+	pin = db.Column(db.String(10), nullable=False)
+	password = db.Column(db.Text, nullable=False)
+	subscribed =db.Column(db.Boolean, nullable=False ,default=False)
+	notifications = db.relationship("Notifications", backref="user", lazy=True)
+
+class Notifications(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+	name = db.Column(db.String(50), nullable=False)
+	date = db.Column(db.String(100), nullable=False, default=date.today().strftime("%d/%m/%Y"))
+	subject = db.Column(db.String(100), nullable=False)
+	body = db.Column(db.Text, nullable=False)
+
+class AllNotifications(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable=False)
+	date = db.Column(db.String(100), nullable=False, default=date.today().strftime("%d/%m/%Y"))
+	subject = db.Column(db.String(100), nullable=False)
+	body = db.Column(db.Text, nullable=False)
+
+#Making Models For databse 
 @app.route('/')
 def home():
-    return render_template('register.html')
-@app.route('/register-user',methods=["POST"])
-def register_user():
+    return render_template('index.html')
+
+@app.route('/subscribe')
+def subscribe():
+	return render_template('subscribe.html')
+@app.route('/subscribe-user',methods=["POST"])
+def subscribe_user():
 	data = request.form
 	session["data"] = data
 	session.permanent = True
@@ -32,5 +79,16 @@ def register_user():
 	pin = data.get('pin')
 	password = data.get('pass')
 	conrfirm_password = data.get('conpass')
-	return render_template('index.html')
+	return rendirect(url_for('short_leads'))
 
+@app.route('/notifications')
+def notifications():
+	# user = Users.query.filter_by(email=session["data"]['email'])
+	# notifications = user.notifications + AllNotifications.query.all()
+	return render_template('notification.html',notification=notifications , date = date.today().strftime("%d/%m/%Y"))
+@app.route('/short-term-leads')
+def short_leads():
+	return render_template('short_leads.html')
+@app.route('/long-term-leads')
+def long_leads():
+	return render_template('long_leads.html')
