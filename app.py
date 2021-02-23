83b61800181caee2e2ee1ad711727c4ab53bbfd2
sharique <sharique15678@gmail.com>
2021-02-23 16:26:55 +0000

    hdggfdf

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config['UPLOAD_FOLDER'] = "/logo"
 app.config['MAX_CONTENT_LENGTH'] = 500 * 1024 * 1024
-
+h = "hello"
 class Leads(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
@@ -66,34 +66,86 @@ def subscribe():
 	return render_template('subscribe.html')
 @app.route('/subscribe-user',methods=["POST"])
 def subscribe_user():
-	data = request.form
-	session["data"] = data
-	session.permanent = True
-	fname = data.get('fname')
-	mname = data.get('mname')
-	lname = data.get('lname')
-	email = data.get('email')
-	add1 = data.get('add1')
-	add2 = data.get('add2')
-	city = data.get('city')
-	state = data.get('state')
-	pin = data.get('pin')
-	password = data.get('pass')
-	conrfirm_password = data.get('conpass')
+    data = request.form
+    if "email" in session :
+        if session['subscribed'] == True :
+            return redirect(url_for('short_leads')
+	if Users.query.filter_by(email=data.get('email')).all():
+	    return render_template('subscribe.html',error="User Is Already Registered. Please Login")
+	if data.get('pass') != data.get('conpass') :
+	    return render_template('subscribe.html',error="Both Passwords Doesnot Match.")
+	user = Users(
+    	fname = data.get('fname'),
+    	mname = data.get('mname'),
+    	lname = data.get('lname'),
+    	email = data.get('email'),
+    	add1 = data.get('add1'),
+    	add2 = data.get('add2'),
+    	city = data.get('city'),
+    	state = data.get('state'),
+    	pin = data.get('pin'),
+    	password = data.get('pass')
+	    )
+	db.session.add(user)
+	db.session.commit()
+	session['email']=data.get('email')
+	session['subscribed']=Users.query.filter_by(email=session['email']).first().subscribed
+	session.permanent =True
 	return redirect(url_for('short_leads'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login/verify',methods=['POST'])
+def login_user():
+    data = request.form
+    if not Users.query.filter_by(email=data.get('email')).all() :
+        return "invalid"
+    user = Users.query.filter_by(email=data.get('email')).first()
+    if user.password != data.get('pass') :
+        return "incorrect"
+    if user.subscribed == False :
+        return "please pay to us"
+    user.subscribed=True
+    db.session.commit()
+    session['email']=user.email
+    session['subscribed']=user.subscribed
+    return redirect(url_for('short_leads'))
 
 @app.route('/notifications')
 def notifications():
-	# user = Users.query.filter_by(email=session["data"]['email'])
-	# notifications = user.notifications + AllNotifications.query.all()
+	user = Users.query.filter_by(email=session['email']).first()
 	notifications = AllNotifications.query.all()
+	list = []
+	for a in user.notifications :
+	    b={"id":a.id,"name":a.name,"date":a.date,"subject":a.subject,"body":a.body}
+	    list.append(b)
+	notifications = notifications + list
 	return render_template('notification.html',notifications=notifications , date = date.today().strftime("%d/%m/%Y"))
+
+@app.route('/short-term-leads')
+def short_leads():
+	return render_template('short_leads.html',leads= Leads.query.filter_by(type="short").all())
+@app.route('/long-term-leads')
+def long_leads():
+	return render_template('long_leads.html',leads= Leads.query.filter_by(type="long").all())
 
 @app.route('/admin')
 def admin():
-	return redirect(url_for('admin_leads'))
-
-
+	return render_template('admin_login.html')
+@app.route('/admin/verify',methods=["POST"])
+def verify_admin():
+    data=request.form
+    if data.get('uname')=="name@example.com" and data.get('pass')=="824210" :
+        session["admin"]=True
+        return redirect(url_for('admin_leads'))
+    else :
+        return "You Are Not Admin"
+@app.route('/admin/logout')
+def admin_logout():
+	session.pop('admin',None)
+	return redirect(url_for('admin'))
 @app.route('/admin/leads')
 def admin_leads():
 	leads = Leads.query.all()
@@ -112,7 +164,7 @@ def confirm_edit_lead(id):
     lead.buy_price = data.get('buy_price')
     lead.sell_price = data.get('sell_price')
     db.session.commit()
-    return redirect(url_for('admin'))
+    return redirect(url_for('admin_leads'))
 
 @app.route('/admin/leads/add')
 def add_lead():
@@ -120,35 +172,85 @@ def add_lead():
 @app.route('/admin/leads/add/verify',methods=['POST'])
 def verify_add_lead():
     data = request.form
-    lead = Leads(
-    	name = data.get('name'),
-	    logo = data.get('logourl'),
-	    type = data.get('type'),
-	    buy_price = str(float(data.get('buy_price'))),
-	    sell_price = str(float(data.get('sell_price'))),
-	    profit = str((float(data.get('sell_price'))-float(data.get('buy_price')))/float(data.get('buy_price'))),
-	    good_for_trade = data.get('good')
-    	)
+    if data.get('good')=='yes' :
+        lead = Leads(
+        	name = data.get('name'),
+    	    logo = data.get('logourl'),
+    	    type = data.get('type'),
+    	    buy_price = str(float(data.get('buy_price'))),
+    	    sell_price = str(float(data.get('sell_price'))),
+    	    profit = str((float(data.get('sell_price'))-float(data.get('buy_price')))/float(data.get('buy_price'))),
+    	    good_for_trade = True
+        	)
+    else :
+        lead = Leads(
+            	name = data.get('name'),
+        	    logo = data.get('logourl'),
+        	    type = data.get('type'),
+        	    buy_price = str(float(data.get('buy_price'))),
+        	    sell_price = str(float(data.get('sell_price'))),
+        	    profit = str((float(data.get('sell_price'))-float(data.get('buy_price')))/float(data.get('buy_price'))),
+        	    good_for_trade = False
+                )
     db.session.add(lead)
     db.session.commit()
-    return data['logourl']
+    return redirect(url_for('admin_leads'))
+@app.route('/admin/leads/delete/<int:id>')
+def delete_lead(id):
+    lead = Leads.query.get(id)
+    db.session.delete(lead)
+    db.session.commit()
+    return redirect(url_for('admin_leads'))
+
 
 
 @app.route('/admin/notifications')
 def admin_notifications():
 	notifications = AllNotifications.query.all() + Notifications.query.all()
 	return render_template('admin_notification.html' , notifications=notifications)
-@app.route('/admin/logout')
-def admin_logout():
-	pass
+
+@app.route('/admin/notifications/add')
+def add_notification():
+    users = Users.query.filter_by(subscribed=True).all()
+    return render_template('admin_create_notification.html',users=users)
+@app.route('/admin/notifications/add/verify',methods=['POST'])
+def verify_add_notification():
+    data = request.form
+    if data.get('users')=="all":
+        notifi=AllNotifications(
+            name=data.get('name'),
+            subject=data.get('subject'),
+            body = data.get('body')
+            )
+        db.session.add(notifi)
+        db.session.commit()
+    else :
+        notifi = Notifications(
+            user_id = int(data.get('users')),
+            name=data.get('name'),
+            subject=data.get('subject'),
+            body = data.get('body')
+            )
+        db.session.add(notifi)
+        db.session.commit()
+    return redirect(url_for('admin_notifications'))
+@app.route('/admin/notifications/delete/personal/<int:id>')
+def delete_personal_notification(id):
+    notifi = Notifications.query.get(id)
+    db.session.delete(notifi)
+    db.session.commit()
+    return redirect(url_for('admin_notifications'))
+@app.route('/admin/notifications/delete/public/<int:id>')
+def delete_public_notification(id):
+    notifi = AllNotifications.query.get(id)
+    db.session.delete(notifi)
+    db.session.commit()
+    return redirect(url_for('admin_notifications'))
 
 
-@app.route('/short-term-leads')
-def short_leads():
-	return render_template('short_leads.html',leads= Leads.query.filter_by(type="short").all())
-@app.route('/long-term-leads')
-def long_leads():
-	return render_template('long_leads.html',leads= Leads.query.filter_by(type="long").all())
+
+
+
 
 @app.route('/add-all')
 def add():
@@ -191,6 +293,25 @@ def add():
 		db.session.add(notifi)
 	db.session.commit()
 	return redirect(url_for('home'))
+@app.route('/delete-all')
+def delete():
+    session.pop('email',None)
+    session.pop('subscribed',None)
+    notis = Notifications.query.all()
+    for noti in notis :
+        db.sesssion.delete(noti)
+    users= Users.query.all()
+    for user in users :
+        db.session.delete(user)
+    anotis = AllNotifications.query.all()
+    for anoti in anotis :
+        db.session.delete(anoti)
+    leads = Leads.query.all()
+    for ld in leads :
+        db.session.delete(ld)
+    db.session.commit()
+    return redirect(url_for('admin'))
+
 if __name__ == "__main__" :
     db.create_all()
     app.run(debug=True)
